f8f84be4b1b3d11b46fdab7ce74047eb6531ed42
seasyqt <kira_man@mail.ru>
2025-12-27 18:44:59 +0300

    task 2-2. Run async function

--- src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
@@ -3,9 +3,13 @@ package course.concurrency.m2_async.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+@EnableAsync
 @Component
 public class AsyncClassTest {
 
@@ -16,11 +20,17 @@ public class AsyncClassTest {
     @Qualifier("applicationTaskExecutor")
     private ThreadPoolTaskExecutor executor;
 
+    @Autowired
+    @Lazy
+    private AsyncClassTest self;
+
+    @Async
     public void runAsyncTask() {
         System.out.println("runAsyncTask: " + Thread.currentThread().getName());
-        internalTask();
+        self.internalTask();
     }
 
+    @Async
     public void internalTask() {
         System.out.println("internalTask: " + Thread.currentThread().getName());
     }
