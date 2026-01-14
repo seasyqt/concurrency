d1a99f112399b5a79f33d8a5fefe7cf68a61d34d
seasyqt <kira_man@mail.ru>
2026-01-14 23:38:45 +0300

    task 3-7. Compare stoppable optimistic and pessimistic blocking

--- src/main/java/course/concurrency/m3_shared/auction/AuctionStoppableOptimistic.java
+++ src/main/java/course/concurrency/m3_shared/auction/AuctionStoppableOptimistic.java
@@ -1,29 +1,47 @@
 package course.concurrency.m3_shared.auction;
 
+import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class AuctionStoppableOptimistic implements AuctionStoppable {
 
-    private Notifier notifier;
+    private final Notifier notifier;
+    private final AtomicMarkableReference<Bid> latestBid;
 
     public AuctionStoppableOptimistic(Notifier notifier) {
+        latestBid = new AtomicMarkableReference<>(new Bid(0L, 0L, 0L), false);
         this.notifier = notifier;
     }
 
-    private Bid latestBid;
 
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            return true;
-        }
-        return false;
+        Bid currentBid;
+        do {
+            if (latestBid.isMarked()) {
+                return false;
+            }
+            currentBid = latestBid.getReference();
+            if (currentBid != null && currentBid.getPrice() >= bid.getPrice()) {
+                return false;
+            }
+
+        } while (!latestBid.compareAndSet(currentBid, bid, false, false));
+
+        notifier.sendOutdatedMessage(currentBid);
+
+        return true;
     }
 
     public Bid getLatestBid() {
-        return latestBid;
+        return latestBid.getReference();
     }
 
     public Bid stopAuction() {
-        return latestBid;
+        Bid current;
+        do {
+            current = latestBid.getReference();
+        } while (!latestBid.attemptMark(current, true));
+
+        return current;
     }
 }
--- src/main/java/course/concurrency/m3_shared/auction/AuctionStoppablePessimistic.java
+++ src/main/java/course/concurrency/m3_shared/auction/AuctionStoppablePessimistic.java
@@ -1,21 +1,33 @@
 package course.concurrency.m3_shared.auction;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class AuctionStoppablePessimistic implements AuctionStoppable {
 
-    private Notifier notifier;
+    private final Notifier notifier;
+    private final ReentrantLock lock = new ReentrantLock();
+    private volatile Bid latestBid = new Bid(0L, 0L, 0L);
+    private volatile boolean isOpenBid = true;
 
     public AuctionStoppablePessimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private Bid latestBid;
-
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            return true;
+        if (isOpenBid && (bid.getPrice() > latestBid.getPrice())) {
+            lock.lock();
+
+            try {
+                if (isOpenBid && (bid.getPrice() > latestBid.getPrice())) {
+                    notifier.sendOutdatedMessage(latestBid);
+                    latestBid = bid;
+                    return true;
+                }
+            } finally {
+                lock.unlock();
+            }
         }
+
         return false;
     }
 
@@ -24,6 +36,12 @@ public class AuctionStoppablePessimistic implements AuctionStoppable {
     }
 
     public Bid stopAuction() {
+        lock.lock();
+        try {
+            isOpenBid = false;
+        } finally {
+            lock.unlock();
+        }
         return latestBid;
     }
 }
