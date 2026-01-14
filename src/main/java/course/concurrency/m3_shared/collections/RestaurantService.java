aa8315a4b4c0f300e9bbd80178d1aeba20033520
seasyqt <kira_man@mail.ru>
2026-01-14 23:54:23 +0300

    task 3-11. Find statistics on threads

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -1,19 +1,24 @@
 package course.concurrency.m3_shared.collections;
 
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class RestaurantService {
 
-    private Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
+    private final Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
         put("A", new Restaurant("A"));
         put("B", new Restaurant("B"));
         put("C", new Restaurant("C"));
     }};
 
-    private Object stat;
+    private final Map<String, AtomicInteger> stat = new ConcurrentHashMap<>() {{
+        put("A", new AtomicInteger(0));
+        put("B", new AtomicInteger(0));
+        put("C", new AtomicInteger(0));
+    }};
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,11 +26,12 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        // your code
+        stat.get(restaurantName).getAndIncrement();
     }
 
     public Set<String> printStat() {
-        // your code
-        return new HashSet<>();
+        return stat.entrySet().stream()
+                .map(element -> element.getKey() + " - " + element.getValue())
+                .collect(Collectors.toSet());
     }
 }
