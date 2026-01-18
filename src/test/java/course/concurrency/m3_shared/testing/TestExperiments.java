e70ade252ecc9d751b51f28d30887bf3e13ca54b
kirilltokarev <kirilltokarev.dev@gmail.com>
2026-01-18 11:33:32 +0300

    Task 4-1 Improve perf test and find blocking thread

--- src/test/java/course/concurrency/m3_shared/testing/TestExperiments.java
+++ src/test/java/course/concurrency/m3_shared/testing/TestExperiments.java
@@ -2,6 +2,10 @@ package course.concurrency.m3_shared.testing;
 
 import org.junit.jupiter.api.RepeatedTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestExperiments {
@@ -21,14 +25,28 @@ public class TestExperiments {
 
     @RepeatedTest(100)
     public void counterShouldFail() {
-        int iterations = 5;
+
+        int threadCount = Runtime.getRuntime().availableProcessors();
+        int iterations = 200;
 
         Counter counter = new Counter();
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(1);
 
         for (int i = 0; i < iterations; i++) {
-            counter.increment();
+
+            executorService.submit(() -> {
+                try {
+                    latch.await();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                counter.increment();
+            });
         }
 
+        latch.countDown();
+        executorService.shutdownNow();
         assertEquals(iterations, counter.get());
     }
 }
