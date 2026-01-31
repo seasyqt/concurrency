b565604e12fdffe42e3380c8fcb4b29c714ba3c3
kirilltokarev <kirilltokarev.dev@gmail.com>
2026-01-31 16:31:33 +0300

    Task 6-2 Add queue LIFO policy and reject executor

--- src/main/java/course/concurrency/m6_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m6_streams/ThreadPoolTask.java
@@ -6,11 +6,23 @@ public class ThreadPoolTask {
 
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        return new ThreadPoolExecutor(1, 1,
+                0, TimeUnit.MILLISECONDS,
+                new LifoBlockingQueue<>());
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(8, 8,
+                60, TimeUnit.SECONDS,
+                new SynchronousQueue<>(), new ThreadPoolExecutor.DiscardPolicy());
+    }
+}
+
+class LifoBlockingQueue<E> extends LinkedBlockingDeque<E> {
+
+    @Override
+    public E take() throws InterruptedException {
+        return super.takeLast();
     }
 }
